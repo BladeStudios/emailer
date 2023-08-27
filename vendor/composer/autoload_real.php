022592f039e357e82cbe50b697044a1507887df5
Blade Studios <bladestudiosit@gmail.com>
2023-08-27 08:08:38 +0200

    ADD install PHP Spreadsheet 1.29

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInit0ce8e3162f22f54ddf58e31647411713
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInit0ce8e3162f22f54ddf58e31647411713', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInit0ce8e3162f22f54ddf58e31647411713', 'loadClassLoader'));
@@ -30,6 +32,18 @@ class ComposerAutoloaderInit0ce8e3162f22f54ddf58e31647411713
         call_user_func(\Composer\Autoload\ComposerStaticInit0ce8e3162f22f54ddf58e31647411713::getInitializer($loader));
 
         $loader->register(true);
+
+        $filesToLoad = \Composer\Autoload\ComposerStaticInit0ce8e3162f22f54ddf58e31647411713::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
+        }
 
         return $loader;
     }
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,7 +6,30 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit0ce8e3162f22f54ddf58e31647411713
 {
+    public static $files = array (
+        '2cffec82183ee1cea088009cef9a6fc3' => __DIR__ . '/..' . '/ezyang/htmlpurifier/library/HTMLPurifier.composer.php',
+    );
+
     public static $prefixLengthsPsr4 = array (
+        'Z' => 
+        array (
+            'ZipStream\\' => 10,
+        ),
+        'P' => 
+        array (
+            'Psr\\SimpleCache\\' => 16,
+            'Psr\\Http\\Message\\' => 17,
+            'Psr\\Http\\Client\\' => 16,
+            'PhpOffice\\PhpSpreadsheet\\' => 25,
+        ),
+        'M' => 
+        array (
+            'Matrix\\' => 7,
+        ),
+        'C' => 
+        array (
+            'Complex\\' => 8,
+        ),
         'B' => 
         array (
             'Bladestudios\\Emailer\\' => 21,
@@ -14,9 +37,48 @@ class ComposerStaticInit0ce8e3162f22f54ddf58e31647411713
     );
 
     public static $prefixDirsPsr4 = array (
+        'ZipStream\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/maennchen/zipstream-php/src',
+        ),
+        'Psr\\SimpleCache\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/psr/simple-cache/src',
+        ),
+        'Psr\\Http\\Message\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/psr/http-message/src',
+            1 => __DIR__ . '/..' . '/psr/http-factory/src',
+        ),
+        'Psr\\Http\\Client\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/psr/http-client/src',
+        ),
+        'PhpOffice\\PhpSpreadsheet\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/phpoffice/phpspreadsheet/src/PhpSpreadsheet',
+        ),
+        'Matrix\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/markbaker/matrix/classes/src',
+        ),
+        'Complex\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/markbaker/complex/classes/src',
+        ),
         'Bladestudios\\Emailer\\' => 
         array (
             0 => __DIR__ . '/../..' . '/src',
+        ),
+    );
+
+    public static $prefixesPsr0 = array (
+        'H' => 
+        array (
+            'HTMLPurifier' => 
+            array (
+                0 => __DIR__ . '/..' . '/ezyang/htmlpurifier/library',
+            ),
         ),
     );
 
@@ -29,6 +91,7 @@ class ComposerStaticInit0ce8e3162f22f54ddf58e31647411713
         return \Closure::bind(function () use ($loader) {
             $loader->prefixLengthsPsr4 = ComposerStaticInit0ce8e3162f22f54ddf58e31647411713::$prefixLengthsPsr4;
             $loader->prefixDirsPsr4 = ComposerStaticInit0ce8e3162f22f54ddf58e31647411713::$prefixDirsPsr4;
+            $loader->prefixesPsr0 = ComposerStaticInit0ce8e3162f22f54ddf58e31647411713::$prefixesPsr0;
             $loader->classMap = ComposerStaticInit0ce8e3162f22f54ddf58e31647411713::$classMap;
 
         }, null, ClassLoader::class);
